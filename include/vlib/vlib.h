be0b6c3ca51ed82a59cbd40a1c67cec31a8b8f1e
vincent <vsallaberry@gmail.com>
2018-03-08 17:44:17 +0100

    imp [vlib.h] add vlib.h including all vlib headers

---
+++ include/vlib/vlib.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2018 Vincent Sallaberry
+ * vlib <https://github.com/vsallaberry/vlib>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/* ------------------------------------------------------------------------
+ * Meta header including all vlib headers.
+ */
+#ifndef VLIB_VLIB_H
+#define VLIB_VLIB_H
+
+#include "slist.h"
+#include "hash.h"
+#include "util.h"
+#include "log.h"
+#include "time.h"
+#include "account.h"
+#include "options.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* ! ifndef *_H */
+
