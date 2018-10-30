33cb024db4b15661ef44a234d8dac513ac387139
vincent <vsallaberry@gmail.com>
2018-10-30 19:52:47 +0100

    new [btree] start binary tree - in progress

---
+++ include/vlib/btree.h
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2018 Vincent Sallaberry
+ * vlib <https://github.com/vsallaberry/vlib>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/* ------------------------------------------------------------------------
+ * Simple bintree utilities.
+ */
+#ifndef VLIB_BTREE_H
+#define VLIB_BTREE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** btree_node_t data compare function, strcmp-like */
+typedef int     (*btree_cmpfun_t)   (const void *, const void *);
+/** btree_node_t data free function, free-like */
+typedef void    (*btree_freefun_t)  (void *);
+
+/** btree_node_t */
+typedef struct btree_node_s {
+    void *                      data;
+    struct btree_node_s *       left;
+    struct btree_node_s *       right;
+} btree_node_t;
+
+/** btree_t */
+typedef struct {
+    struct btree_node_t *       root;
+    btree_cmpfun_t              cmp;
+    btree_freefun_t             free;
+} btree_t;
+
+/** btree_visitfun_t return value */
+enum {
+    BVS_GO_LEFT     = -24, /* only go to left node, but continue visit */
+    BVS_GO_RIGHT    = -23, /* only go to right node, but continue visit */
+    BVS_SKIP        = -22, /* skip this node, but continue visit */
+    BVS_CONT        = -21, /* continue visit */
+    BVS_ERROR       = -1,  /* stop visit, report error */
+    BVS_FINISHED    = 0,   /* stop visit, report success */
+} btree_visit_status_t;
+
+/** btree_node_t visit function called on each node by btree_visit()
+ * @param tree the tree beiing visited, cannot be NULL (ensured by btree_visit())
+ * @param node the node beiing visited, cannot be NULL (ensured by btree_visit())
+ * @param data specific data for visit function
+ * @return btree_visit_status_t
+ */
+typedef int     (*btree_visitfun_t) (btree_t * tree, btree_node_t * node, void * data);
+
+/** btree_create() */
+btree_t *       btree_create(
+                    btree_cmpfun_t      cmpfun,
+                    btree_freefun_t     freefun);
+
+/** btree_insert() */
+btree_node_t *  btree_insert(
+                    btree_t *           tree,
+                    void *              data);
+
+/** btree_free() */
+void            btree_free(
+                    btree_t *           tree);
+
+/** btree_find() */
+void *          btree_find(
+                    btree_t *           tree,
+                    const void *        data);
+
+/** btree_visit() */
+int             btree_visit(
+                    btree_t *           tree,
+                    btree_visitfun_t    visit,
+                    void *              visit_data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ! ifndef *_H */
+
---
+++ src/btree.c
@@ -0,0 +1,173 @@
+/*
+ * Copyright (C) 2018 Vincent Sallaberry
+ * vlib <https://github.com/vsallaberry/vlib>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/* ------------------------------------------------------------------------
+ * Simple bintree utilities.
+ */
+#include <stdlib.h>
+
+#include "vlib/btree.h"
+#include "vlib/slist.h"
+
+typedef struct {
+    btree_node_t *  node;
+    void *          data;
+} btree_visit_insert_t;
+
+static void btree_avl_rotate(btree_t * tree);
+
+static btree_node_t * btree_node_alloc(btree_t * tree) {
+    (void)tree;
+    return malloc(sizeof(btree_node_t));
+}
+
+static void btree_node_free(btree_t * tree, btree_node_t * node) {
+    if (tree && node) {
+        if (tree->free) {
+            tree->free(node->data);
+        }
+        free(node);
+    }
+}
+
+static int btree_visit_insert(btree_t * tree, btree_node_t * node, void * data) {
+    btree_visit_insert_t *      insert_data = (btree_visit_insert_t *) data;
+    btree_node_t *              new;
+    btree_node_t **             parent;
+
+    if (tree->cmp(data, node->data) <= 0) {
+        /* go left */
+        if (node->left != NULL && tree->cmp(data, node->left->data) <= 0) { // FIXME
+            /* need to visit left node because its value is greater than data */
+            return BVS_GO_LEFT;
+        }
+        /* left is null, we insert on node->left */
+        parent = &node->left;
+    } else {
+        /* go right */
+        if (node->right != NULL && tree->cmp(data, node->right->data) > 0) { // FIXME
+            /* need to visit right node because its value is smaller than data */
+            return BVS_GO_RIGHT;
+        }
+        /* right is null, we insert on node->right */
+        parent = &node->right;
+    }
+    new = btree_node_alloc(tree);
+    if (node == NULL)
+        return BVS_ERROR;
+    new->left = NULL; //FIXME
+    new->right = NULL; //FIXME
+    new->data = insert_data->data;
+    (*parent) = new;
+    insert_data->node = new;
+    return BVS_FINISHED;
+}
+
+static int btree_visit_free(btree_t * tree, btree_node_t * node, void * data) {
+    (void) data;
+    btree_free_node(tree, node);
+    return BVS_CONT;
+}
+
+btree_t *       btree_create(
+                    btree_cmpfun_t      cmpfun,
+                    btree_freefun_t     freefun) {
+    btree_t * tree;
+
+    if (cmpfun == NULL) {
+        return NULL; // bin tree is based on node comparisons.
+    }
+    tree = malloc(sizeof(btree_t));
+    if (tree == NULL) {
+        return NULL;
+    }
+    tree->root = NULL;
+    tree->cmp = cmpfun;
+    tree->free = freefun;
+    return tree;
+}
+
+btree_node_t *  btree_insert(
+                    btree_t *           tree,
+                    void *              data) {
+    btree_visit_insert_t    insert_data;
+
+    if (tree == NULL) {
+        return NULL;
+    }
+    insert_data.data = data;
+    if (btree_visit(tree, btree_visit_insert, &insert_data) == BVS_FINISHED) {
+        return insert_data.node;
+    }
+    return NULL;
+}
+
+void            btree_free(
+                    btree_t *           tree) {
+    if (tree == NULL) {
+        return ;
+    }
+    btree_visit(tree, btree_visit_free, NULL);
+}
+
+void *          btree_find(
+                    btree_t *           tree,
+                    const void *        data) {
+    return NULL;
+}
+
+int             btree_visit(
+                    btree_t *           tree,
+                    btree_visitfun_t    visit,
+                    void *              visit_data) {
+    slist_t *   stack = NULL;
+    int         ret = BVS_FINISHED;
+
+    if (tree == NULL || visit == NULL) {
+        return BVS_ERROR;
+    }
+    stack = slist_prepend(stack, tree->root);
+    while (stack != NULL) {
+        btree_node_t *      node = (btree_node_t *) stack->data;
+        btree_node_t *      right = node->right;
+        btree_node_t *      left = node->left;
+        /* visit the current node */
+        ret = visit(tree, node, visit_data);
+        /* pop current node from the stack */
+        stack = slist_remove_ptr(stack->data, NULL);
+        /* stop on error or when visit goal is accomplished */
+        if (ret == BVS_ERROR || ret == BVS_FINISHED) {
+            break ;
+        }
+        /* push node->left if required */
+        if (ret == BVS_GO_LEFT || ret == BVS_CONT) {
+            stack = slist_prepend(stack, left);
+        }
+        /* push node->right if required */
+        if (ret == BVS_GO_RIGHT || ret == BVS_CONT) {
+            stack = slist_prepend(stack, right);
+        }
+    }
+    slist_free(stack, NULL);
+    return ret != BVS_ERROR ? BVS_FINISHED : BVS_ERROR;
+}
+
+static void btree_avl_rotate(btree_t * tree) {
+
+}
+
