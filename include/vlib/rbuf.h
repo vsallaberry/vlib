ce56d7e3e5e9cce00132622e4469187f84f9b48c
vincent <vsallaberry@gmail.com>
2018-11-04 16:35:49 +0100

    new [ringbuffer] add rbuf supporting stack(lifo) and queue(fifo) features

---
+++ include/vlib/rbuf.h
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2018 Vincent Sallaberry
+ * vlib <https://github.com/vsallaberry/vlib>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/* ------------------------------------------------------------------------
+ * Simple ring buffer utilities, supporting stack(lifo) & queue(fifo) features.
+ *   rbuf_t * rbuf = rbuf_create(10, RBF_DEFAULT);
+ *   rbuf_push(rbuf, (void*) 1l);
+ *   rbuf_push(rbuf, (void*) 2l);
+ *   while (rbuf_size(rbuf) != 0) printf(">%ld\n", (long)rbuf_pop(rbuf));
+ *      // lifo: prints 2, 1
+ *   rbuf_push(rbuf, (void*) 1l);
+ *   rbuf_push(rbuf, (void*) 2l);
+ *   while (rbuf_size(rbuf) != 0) printf(">%ld\n", (long)rbuf_dequeue(rbuf));
+ *     // fifo: prints 1, 2
+ *   rbuf_free(rbuf);
+ */
+#ifndef VLIB_RBUF_H
+#define VLIB_RBUF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*****************************************************************************/
+#define VLIB_RBUF_SZ   64
+
+typedef enum {
+    RBF_NONE            = 0,
+    RBF_OVERWRITE       = 1 << 0, /* when buffer is full, overwrite instead of increasing size */
+    RBF_SHRINK_ON_RESET = 1 << 1, /* restore initial buffer size when calling rbuf_reset() */
+    RBF_DEFAULT         = RBF_NONE,
+} rbuf_flags_t;
+
+/** opaque struct rbuf_t/rbuf_s */
+typedef struct rbuf_s   rbuf_t;
+
+/*****************************************************************************/
+
+/** create a ring buffer of initial size <max_size>.
+ * unless RBF_OVERWRITE is ON, max_size is doubled in rbuf_push() if buffer is full
+ * @param max_size the maximum number of elements in the buffer, which can increase if
+ *        RBF_OVERWRITE is not given
+ * @param flags the buffer configuration @see rbuf_flags_t
+ * @return the new created buffer or NULL on error */
+rbuf_t *        rbuf_create(
+                    size_t          max_size,
+                    rbuf_flags_t    flags);
+
+/** empty the buffer and restore its initial size if RBF_SHRINK_ON_RESET is given
+ * @return 0 on success and -1 on error (bad paramaters, out of memory) */
+int             rbuf_reset(
+                    rbuf_t *        rbuf);
+
+/** clean buffer data and release memory
+ * @param rbuf the buffer */
+void            rbuf_free(
+                    rbuf_t *        rbuf);
+
+/** number of element in the buffer
+ * @param rbuf the buffer
+ * @return number of elements in the buffer */
+size_t          rbuf_size(
+                    rbuf_t *        rbuf);
+
+/** push an element at end of the buffer: compatible with lifo(stack)/fifo(queue)
+ * @param rbuf the buffer
+ * @param data the element to be pushed
+ * @return 0 on success, -1 if element cannot be pushed (bad parameters, out of memory). */
+int             rbuf_push(
+                    rbuf_t *        rbuf,
+                    void *          data);
+
+/** Last element. This one would be returned by rbuf_pop()
+ * @return the last element or NULL on error with errno set (errno is NOT changed on success). */
+void *          rbuf_top(
+                    rbuf_t *        rbuf);
+
+/** remove and return last element of the buffer : LIFO(stack) mode.
+ * @return the popped element or NULL with errno set (errno is NOT changed on success). */
+void *          rbuf_pop(
+                    rbuf_t *        rbuf);
+
+/** First element. This one would be returned by rbuf_dequeue()
+ * @return the first element or NULL with errno set (errno is NOT changed on success). */
+void *          rbuf_bottom(
+                    rbuf_t *        rbuf);
+
+/** remove and return first element of the buffer : FIFO(queue) mode.
+ * @return the dequeued element or NULL with errno set (errno is NOT changed on success). */
+void *          rbuf_dequeue(
+                    rbuf_t *        rbuf);
+
+/** get the index-th element of the buffer
+ * @return the index-th element or NULL with errno set (errno is NOT changed on success). */
+void *          rbuf_get(
+                    rbuf_t *        rbuf,
+                    size_t          index);
+
+/** set the index-th element in the buffer.
+ * if RBF_OVERWRITE is ON, the buffer is increased so that index will fit.
+ * @return 0 on success or -1 on error */
+int             rbuf_set(
+                    rbuf_t *        rbuf,
+                    size_t          index,
+                    void *          data);
+
+/*****************************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ! ifndef *_H */
+
+
---
+++ src/rbuf.c
@@ -0,0 +1,290 @@
+/*
+ * Copyright (C) 2018 Vincent Sallaberry
+ * vlib <https://github.com/vsallaberry/vlib>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/* ------------------------------------------------------------------------
+ * Simple ring buffer utilities, supporting stack(lifo) & queue(fifo) features.
+ */
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+
+#include "vlib/rbuf.h"
+#include "vlib/log.h"
+
+#include "vlib_private.h"
+
+/*****************************************************************************/
+
+#define RBUF_IS_EMPTY(rbuf)     ((rbuf)->end >= (rbuf)->max_size)
+#define RBUF_EMPTY              SIZE_MAX
+
+struct rbuf_s {
+    void **         tab;
+    size_t          init_size;
+    size_t          max_size;
+    rbuf_flags_t    flags;
+    size_t          start;
+    size_t          end;
+};
+
+/*****************************************************************************/
+rbuf_t *        rbuf_create(
+                    size_t          max_size,
+                    rbuf_flags_t    flags) {
+    rbuf_t * rbuf;
+
+    if (max_size == 0)  {
+        return NULL;
+    }
+    if ((rbuf = malloc(sizeof(rbuf_t))) == NULL) {
+        return NULL;
+    }
+    if ((rbuf->tab = malloc(sizeof(void *) * max_size)) == NULL) {
+        free(rbuf);
+        return NULL;
+    }
+    rbuf->max_size = max_size;
+    rbuf->init_size = max_size;
+    rbuf->flags = flags;
+    rbuf_reset(rbuf);
+    return rbuf;
+}
+/*****************************************************************************/
+int             rbuf_reset(
+                    rbuf_t *        rbuf) {
+    if (rbuf == NULL) {
+        return -1;
+    }
+
+    rbuf->start = 0;
+    rbuf->end = RBUF_EMPTY;
+
+    if ((rbuf->flags & RBF_SHRINK_ON_RESET) != 0 && rbuf->max_size > rbuf->init_size) {
+        void * new = realloc(rbuf->tab, sizeof(void *) * rbuf->init_size);
+        if (new == NULL) {
+            return -1;
+        }
+        rbuf->tab = new;
+        rbuf->max_size = rbuf->init_size;
+    }
+
+    return 0;
+}
+/*****************************************************************************/
+void            rbuf_free(
+                    rbuf_t *        rbuf) {
+    if (rbuf == NULL) {
+        return ;
+    }
+    if (rbuf->tab != NULL) {
+        free(rbuf->tab);
+    }
+    free(rbuf);
+}
+/*****************************************************************************/
+size_t          rbuf_size(
+                    rbuf_t *        rbuf) {
+    ssize_t size;
+
+    if (rbuf == NULL || RBUF_IS_EMPTY(rbuf) || rbuf->start >= rbuf->max_size) {
+        return 0;
+    }
+    if ((size = 1 + rbuf->end - rbuf->start) <= 0) {
+        size += rbuf->max_size;
+    }
+    return size;
+}
+/*****************************************************************************/
+int             rbuf_push(
+                    rbuf_t *        rbuf,
+                    void *          data) {
+    if (rbuf == NULL || rbuf->start >= rbuf->max_size) {
+        errno = EINVAL;
+        return -1;
+    }
+    LOG_DEBUG(g_vlib_log, "rbuf_push(%lu) start:%lu end:%lu maxsize:%lu",
+              (size_t)data, rbuf->start, rbuf->end, rbuf->max_size);
+
+    if (RBUF_IS_EMPTY(rbuf)) {
+        rbuf->tab[(rbuf->end = rbuf->start)] = data;
+        return 0;
+    }
+    ++rbuf->end;
+    if (rbuf->end == rbuf->max_size) {
+        rbuf->end = 0;
+    }
+    if (rbuf->end == rbuf->start) {
+        /* start is reached -> buffer is full */
+        if ((rbuf->flags & RBF_OVERWRITE) == 0) {
+            /* OVERWRITE MODE is OFF -> increase buffer size */
+            void * new = realloc(rbuf->tab, sizeof(void *) * (rbuf->max_size * 2));
+            if (new == NULL) {
+                return -1;
+            }
+            rbuf->tab = new;
+            if (rbuf->end > 0) {
+                /* start == end but end > 0 meaning that data in range [0,end-1] must be moved */
+                LOG_DEBUG(g_vlib_log, "memcpy realloc psh:%ld start:%lu end:%lu maxsz:%lu",
+                          (long) data, rbuf->start, rbuf->end, rbuf->max_size);
+                memcpy(rbuf->tab + rbuf->max_size, rbuf->tab, rbuf->end * sizeof(void *));
+            }
+            rbuf->end = rbuf->max_size + rbuf->end;
+            rbuf->max_size *= 2;
+        } else {
+            /* OVERWRITE MODE is ON -> lose the bottom element */
+            rbuf->start++;
+            if (rbuf->start == rbuf->max_size) {
+                rbuf->start = 0;
+            }
+        }
+    }
+    rbuf->tab[rbuf->end] = data;
+
+    LOG_DEBUG(g_vlib_log, "rbuf_push(%lu) PUSHED. start:%lu end:%lu maxsize:%lu",
+              (size_t)data, rbuf->start, rbuf->end, rbuf->max_size);
+
+    return 0;
+}
+/*****************************************************************************/
+void *          rbuf_top(
+                    rbuf_t *        rbuf) {
+    if (rbuf == NULL || rbuf->start >= rbuf->max_size) {
+        errno = EINVAL;
+        return NULL;
+    }
+    if (RBUF_IS_EMPTY(rbuf)) {
+        errno = ENOENT;
+        return NULL;
+    }
+    return rbuf->tab[rbuf->end];
+}
+/*****************************************************************************/
+void *          rbuf_pop(
+                    rbuf_t *        rbuf) {
+    void *  ret;
+
+    if (rbuf == NULL || rbuf->start >= rbuf->max_size) {
+        errno = EINVAL;
+        return NULL;
+    }
+    if (RBUF_IS_EMPTY(rbuf)) {
+        errno = ENOENT;
+        return NULL;
+    }
+    ret = rbuf->tab[rbuf->end];
+    if (rbuf->end == rbuf->start) {
+        rbuf->end = RBUF_EMPTY;
+        //rbuf->start = 0;
+        return ret;
+    }
+    if (rbuf->end-- == 0) {
+        rbuf->end = rbuf->max_size - 1;
+    }
+    return ret;
+}
+/*****************************************************************************/
+void *          rbuf_bottom(
+                    rbuf_t *        rbuf) {
+    if (rbuf == NULL || rbuf->start >= rbuf->max_size) {
+        errno = EINVAL;
+        return NULL;
+    }
+    if (RBUF_IS_EMPTY(rbuf)) {
+        errno = ENOENT;
+        return NULL;
+    }
+    return rbuf->tab[rbuf->start];
+}
+/*****************************************************************************/
+void *          rbuf_dequeue(
+                    rbuf_t *        rbuf) {
+    void * ret;
+
+    if (rbuf == NULL || rbuf->start >= rbuf->max_size) {
+        errno = EINVAL;
+        return NULL;
+    }
+    if (RBUF_IS_EMPTY(rbuf)) {
+        errno = ENOENT;
+        return NULL;
+    }
+    ret = rbuf->tab[rbuf->start];
+
+    LOG_DEBUG(g_vlib_log, "rbuf_dequeue = %lu start:%lu end:%lu maxsize:%lu",
+              (size_t)ret, rbuf->start, rbuf->end, rbuf->max_size);
+
+    if (rbuf->end == rbuf->start) {
+        rbuf->end = RBUF_EMPTY;
+        //rbuf->start = 0;
+        return ret;
+    }
+    if (++rbuf->start >= rbuf->max_size) {
+        rbuf->start = 0;
+    }
+    return ret;
+}
+/*****************************************************************************/
+void *          rbuf_get(
+                    rbuf_t *        rbuf,
+                    size_t          index) {
+    if (rbuf == NULL || rbuf->start >= rbuf->max_size) {
+        errno = EINVAL;
+        return NULL;
+    }
+    if (RBUF_IS_EMPTY(rbuf) || index >= rbuf_size(rbuf)) {
+        errno = ENOENT;
+        return NULL;
+    }
+    return rbuf->tab[(rbuf->start + index) % rbuf->max_size];
+}
+/*****************************************************************************/
+int             rbuf_set(
+                    rbuf_t *        rbuf,
+                    size_t          index,
+                    void *          data) {
+    size_t real_index;
+
+    if (rbuf == NULL || rbuf->start >= rbuf->max_size) {
+        errno = EINVAL;
+        return -1;
+    }
+    if (index >= rbuf->max_size) {
+        if ((rbuf->flags & RBF_OVERWRITE) != 0) {
+            errno = ENOMEM;
+            return -1;
+        }
+        /* push null until buffer is enough large for index */
+        while (rbuf->max_size - 1 < index) {
+            if (rbuf_push(rbuf, NULL) != 0) {
+                return -1;
+            }
+        }
+    }
+    real_index = (rbuf->start + index) % rbuf->max_size;
+    rbuf->tab[real_index] = data;
+
+    if (RBUF_IS_EMPTY(rbuf) || index >= rbuf_size(rbuf)) {
+        rbuf->end = real_index;
+    }
+
+    return 0;
+}
+/*****************************************************************************/
+
+
