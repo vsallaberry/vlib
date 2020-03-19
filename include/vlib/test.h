6d010f8d10b193265d4a0858ccc8ef7c84754e2a
vincent <vsallaberry@gmail.com>
2020-03-19 16:15:03 +0100

    new [test] simple unit test utilities

---
+++ include/vlib/test.h
@@ -0,0 +1,233 @@
+/*
+ * Copyright (C) 2020 Vincent Sallaberry
+ * vlib <https://github.com/vsallaberry/vlib>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/* ------------------------------------------------------------------------
+ * Simple unit tests utilities.
+ */
+#ifndef VLIB_TEST_H
+#define VLIB_TEST_H
+
+#ifdef __cplusplus
+# include <cstdlib>
+# include <cstdarg>
+#else
+# include <stdlib.h>
+# include <stdarg.h>
+#endif
+
+#include "vlib/log.h"
+#include "vlib/logpool.h"
+#include "vlib/time.h"
+#include "vlib/slist.h"
+
+/* ************************************************************************ */
+
+/** opaque testpool_t */
+typedef struct testpool_s       testpool_t;
+
+typedef struct {
+    testpool_t *    testpool;
+    unsigned int    flags;
+    log_t *         log;
+    char *          name;
+    slist_t *       results;
+    unsigned long   n_tests;
+    unsigned long   n_ok;
+    unsigned long   n_errors;
+    BENCH_TM_DECL   (tm_bench);
+    BENCH_DECL      (cpu_bench);
+} testgroup_t;
+
+typedef struct {
+    testgroup_t *   testgroup;
+    char *          checkname;
+    char *          msg;
+    unsigned long   id;
+    char *          file;
+    char *          func;
+    unsigned int    line;
+    int             success;
+    BENCH_TM_DECL   (tm_bench);
+    BENCH_DECL      (cpu_bench);
+} testresult_t;
+
+/** tests_create() flags, will be copied to group on group creation,
+ * then the group can update its own flags independently */
+typedef enum {
+    TPF_NONE            = 0,
+    TPF_LOGTRUEPREFIX   = 1 << 0,
+    TPF_STORE_RESULTS   = 1 << 1,
+    TPF_STORE_ERRORS    = 1 << 2,
+    TPF_BENCH_RESULTS   = 1 << 3,
+    TPF_DEFAULT         = (TPF_STORE_ERRORS | TPF_NONE),
+    TPF_INTERNAL        = 1 << 16
+} testpool_flags_t;
+
+/** DEFAULT TESTPOOL LOGPREFIX if TPF_LOGTRUEPREFIX is not given to tests_create() */
+#define TESTPOOL_LOG_PREFIX     "tests"
+
+/* ************************************************************************
+ * Usage:
+ *  // create testpool
+ *  testpool_t * testpool = tests_create(NULL, TPF_DEFAULT);
+ *  testgroup_t * test;
+ *  unsigned int nerrors = 0;
+ *  test = TEST_START(testpool, "TEST01");
+ *  TEST_CHECK(test, "CHECK01", 1 == 1);
+ *  TEST_CHECK(test, "CHECK02", 1 == 0);
+ *  if (test) test->flags |= TPF_STORE_RESULTS | TPF_BENCH_RESULTS;
+ *  TEST_CHECK(test, "CHECK03", 1 == 1);
+ *  TEST_CHECK2(test, LOG_LVL_INFO, 1 == 0, "CHECK04 checking %s", "something");
+ *  nerrors += TEST_END(test);
+ *  tests_print(testpool, TPR_DEFAULT);
+ *  tests_free(testpool);
+ */
+/* ************************************************************************ */
+
+/** start a group of tests: return a (testgroup_t *) not be shared with other threads */
+#define TEST_START(                                     \
+            /*(testpool_t *)*/      _TESTPOOL,          \
+            /*(const char *)*/      _TESTNAME)          \
+        TEST_START2(_TESTPOOL, _TESTNAME, "%s", "")
+
+/** end a group of tests: return number of errors */
+#define TEST_END(                                       \
+            /*(testgroup_t *)*/     _TESTGROUP)         \
+        TEST_END2(_TESTGROUP, "%s", "")
+
+/** perform a check on current test group, no return (void) */
+#define TEST_CHECK(                                     \
+            /*(testgroup_t *)*/     _TESTGROUP,         \
+            /*String constant*/     _msg,               \
+            /*(Boolean Expr.)*/     _CHECKING)          \
+        TEST_CHECK2(_TESTGROUP, LOG_LVL_VERBOSE, _CHECKING, _msg "%s", "")
+
+/* ************************************************************************ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* ************************************************************************ */
+
+/** create a testpool
+ * @param logpool the pool to use gettings logs, can be NULL
+ * @param flags bit combination of testpool_flags_t
+ * @return allocated testpool to be freed with test_free() */
+testpool_t *            tests_create(
+                            logpool_t *         logs,
+                            unsigned int        flags);
+
+/** get log instance assiciated with current test
+ * @param tests the test pool.
+ * @param testname the name of test
+ * @return log instance or NULL on error */
+log_t *                 tests_getlog(
+                            testpool_t *        tests,
+                            const char *        testname);
+
+/** free a testpool
+ * @param tests the test pool
+ * @return 0 on success, negative on error */
+int                     tests_free(
+                            testpool_t *        tests);
+/** tests_print() flags */
+typedef enum {
+    TPR_NONE            = 0,
+    TPR_PRINT_GROUPS    = 1 << 0,
+    TPR_PRINT_ERRORS    = 1 << 1,
+    TPR_PRINT_OK        = 1 << 2,
+    TPR_DEFAULT         = (TPR_PRINT_GROUPS | TPR_PRINT_ERRORS | TPR_PRINT_OK)
+} tests_print_flags_t;
+
+/** print a testpool
+ * @param tests the test pool
+ * @param flags a bit combination of tests_print_flags_t
+ * @return negative value on error */
+int                     tests_print(
+                            testpool_t *        tests,
+                            unsigned int        flags);
+
+/* ************************************************************************ */
+/* INTERNAL */
+/* ************************************************************************ */
+
+/** code for TEST_START macro: use TEST_START rather than this */
+testgroup_t *           tests_start(
+                            testpool_t *        tests,
+                            const char *        testname);
+
+/** code for TEST_END macro: use TEST_END rather than this */
+unsigned long           tests_end(
+                            testgroup_t *       testgroup);
+
+/** code for TEST_CHECK macro: use TEST_CHECK rather than this */
+int                     tests_check(
+                            testresult_t *      result,
+                            const char *        func,
+                            const char *        file,
+                            int                 line);
+
+/* ************************************************************************ */
+#ifdef __cplusplus
+}
+#endif
+
+/* ************************************************************************ */
+#define TEST_START2(_TESTPOOL, _TESTNAME, _fmt, ...)                        \
+    ( (_TESTPOOL) != NULL && (_TESTNAME) != NULL                            \
+      && (LOG_INFO(tests_getlog(_TESTPOOL, _TESTNAME), ">>> %s tests" _fmt, \
+                   _TESTNAME, __VA_ARGS__) || 1)                            \
+          ? tests_start(_TESTPOOL, _TESTNAME) : NULL)
+
+#define TEST_END2(_TESTGROUP, _fmt, ...)                                    \
+    ( (_TESTGROUP) != NULL                                                  \
+       && (LOG_INFO((_TESTGROUP)->log,                                      \
+                "<- %s tests: ending with %lu error%s" _fmt,                \
+                (_TESTGROUP)->name, (_TESTGROUP)->n_errors,                 \
+                (_TESTGROUP)->n_errors > 1 ? "s" : "", __VA_ARGS__) || 1)   \
+           ? tests_end(_TESTGROUP) : 1)
+
+#define TEST_CHECK2(_TESTGROUP, _loglevel, _CHECKING, _fmt, ...)            \
+        do {                                                                \
+            testresult_t    result = { .testgroup = _TESTGROUP, .msg = _fmt,\
+                                       .checkname = #_CHECKING };           \
+            if ((_TESTGROUP) == NULL) break ;                               \
+            if (((_TESTGROUP)->flags & TPF_BENCH_RESULTS) != 0) {           \
+                BENCH_TM_START(result.tm_bench);                            \
+                BENCH_START(result.cpu_bench);                              \
+            }                                                               \
+            result.success = (_CHECKING);                                   \
+            if (((_TESTGROUP)->flags & TPF_BENCH_RESULTS) != 0) {           \
+                BENCH_STOP(result.cpu_bench);                               \
+                BENCH_TM_STOP(result.tm_bench);                             \
+            }                                                               \
+            if (result.success) {                                           \
+                if (LOG_CAN_LOG((_TESTGROUP)->log, _loglevel)) {            \
+                    vlog(_loglevel, (_TESTGROUP)->log, __FILE__, __func__, __LINE__, \
+                         "%s: OK: " _fmt "(" #_CHECKING ")",   \
+                         (_TESTGROUP)->name, __VA_ARGS__);                  \
+                }                                                           \
+            } else {                                                        \
+                LOG_ERROR((_TESTGROUP)->log, "%s: ERROR " _fmt "(" #_CHECKING ")",\
+                          (_TESTGROUP)->name, __VA_ARGS__);                 \
+            }                                                               \
+            tests_check(&result, __func__, __FILE__, __LINE__);             \
+        } while(0)
+
+/* ************************************************************************ */
+#endif /* ! ifndef *_H */
+
---
+++ src/test.c
@@ -0,0 +1,304 @@
+/*
+ * Copyright (C) 2020 Vincent Sallaberry
+ * vlib <https://github.com/vsallaberry/vlib>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/* ------------------------------------------------------------------------
+ * Simple unit tests utilities.
+ */
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "vlib/avltree.h"
+#include "vlib/log.h"
+#include "vlib/logpool.h"
+#include "vlib/job.h"
+#include "vlib/time.h"
+#include "vlib/slist.h"
+
+#include "vlib/test.h"
+
+#include "vlib_private.h"
+
+/* ************************************************************************ */
+
+struct testpool_s {
+    avltree_t *         tests;
+    logpool_t *         logs;
+    unsigned int        flags;
+    pthread_rwlock_t    rwlock;
+};
+
+enum {
+    TPF_FREE_LOGPOOL    = TPF_INTERNAL,
+};
+
+/* ************************************************************************ */
+static void tests_result_free(void * vdata) {
+    testresult_t * result = (testresult_t *) vdata;
+
+    if (result != NULL) {
+        if (result->checkname != NULL)
+            free(result->checkname);
+        if (result->msg != NULL)
+            free(result->msg);
+        if (result->file != NULL)
+            free(result->file);
+        if (result->func != NULL)
+            free(result->func);
+        free(result);
+    }
+}
+static void tests_group_free(void * vdata) {
+    testgroup_t * group = (testgroup_t *) vdata;
+
+    if (group != NULL) {
+        if (group->name != NULL)
+            free(group->name);
+        if (group->results != NULL)
+            slist_free(group->results, tests_result_free);
+        free(group);
+    }
+}
+static int tests_group_cmp(const void * v1, const void * v2) {
+    testgroup_t * g1 = (testgroup_t *) v1;
+    testgroup_t * g2 = (testgroup_t *) v2;
+
+    if (g1 == g2) {
+        return 0;
+    }
+    if (g1 == NULL || g2 == NULL) {
+        return g1 - g2;
+    }
+    return strcmp(g1->name, g2->name);
+}
+
+/* ************************************************************************ */
+testpool_t *            tests_create(
+                            logpool_t *         logs,
+                            unsigned int        flags) {
+    (void) flags;
+    testpool_t * tests = calloc(1, sizeof(testpool_t));
+
+    if (tests == NULL) {
+        return NULL;
+    }
+    pthread_rwlock_init(&tests->rwlock, NULL);
+    tests->flags = flags;
+
+    if (logs == NULL) {
+        tests->logs = logpool_create();
+        tests->flags |= TPF_FREE_LOGPOOL;
+    } else {
+        tests->logs = logs;
+    }
+    tests->tests = avltree_create(AFL_DEFAULT, tests_group_cmp, tests_group_free);
+
+    if (tests->logs == NULL || tests->tests == NULL) {
+        tests_free(tests);
+        return NULL;
+    }
+
+    return tests;
+}
+
+/* ************************************************************************ */
+int                     tests_free(
+                            testpool_t *        tests) {
+    if (tests != NULL) {
+        pthread_rwlock_wrlock(&(tests->rwlock));
+        if ((tests->flags & TPF_FREE_LOGPOOL) != 0) {
+            logpool_free(tests->logs);
+        }
+        avltree_free(tests->tests);
+        tests->tests = NULL;
+        pthread_rwlock_unlock(&(tests->rwlock));
+        pthread_rwlock_destroy(&(tests->rwlock));
+        free(tests);
+        return 0;
+    }
+    return -1;
+}
+
+/* ************************************************************************ */
+typedef struct {
+    log_t *         log;
+    unsigned int    flags;
+} tests_print_visit_t;
+static avltree_visit_status_t   tests_printgroup_visit(
+                                    avltree_t *                         tree,
+                                    avltree_node_t *                    node,
+                                    const avltree_visit_context_t *     context,
+                                    void *                              user_data) {
+    tests_print_visit_t *   data    = (tests_print_visit_t *) user_data;
+    testgroup_t *           group   = (testgroup_t *) node->data;
+    (void) context;
+    (void) tree;
+
+    if (group != NULL) {
+        if ((data->flags & TPR_PRINT_GROUPS) != 0) {
+            LOG_INFO(data->log, "%s - %lu/%lu OK, %lu error%s, duration: %lums (cpu:%lums)",
+                group->name, group->n_ok, group->n_tests, group->n_errors,
+                group->n_errors > 1 ? "s" : "",
+                BENCH_TM_GET(group->tm_bench), BENCH_GET(group->cpu_bench));
+        }
+        if ((data->flags & (TPR_PRINT_ERRORS | TPR_PRINT_OK)) != 0) {
+            SLIST_FOREACH_DATA(group->results, result, testresult_t *) {
+                if (result != NULL
+                    &&  (   ((data->flags & TPR_PRINT_ERRORS) != 0  && result->success == 0)
+                         || ((data->flags & TPR_PRINT_OK) != 0      && result->success != 0))) {
+                        LOG_INFO(data->log, "  [%s] %s/%lu: %s(%s), duration: %lums (cpu:%lums), %s():%s:%u",
+                                result->success ? "OK    " : "FAILED",
+                                result->testgroup->name, result->id, result->msg, result->checkname,
+                                BENCH_TM_GET(result->tm_bench), BENCH_GET(result->cpu_bench),
+                                result->func, result->file, result->line);
+                }
+            }
+        }
+    }
+    return AVS_CONTINUE;
+}
+
+/* ************************************************************************ */
+int                     tests_print(
+                            testpool_t *        tests,
+                            unsigned int        flags) {
+    tests_print_visit_t data = { .flags = flags };
+    int ret;
+
+    if (tests == NULL) {
+        return -1;
+    }
+
+    if ((flags & (TPR_PRINT_GROUPS | TPR_PRINT_ERRORS | TPR_PRINT_OK)) == 0) {
+        return 0;
+    }
+
+    /* ---------------------------------------------------------------------*/
+    pthread_rwlock_rdlock(&(tests->rwlock));
+    data.log = logpool_getlog(tests->logs, TESTPOOL_LOG_PREFIX, LPG_DEFAULT);
+    ret = avltree_visit(tests->tests, tests_printgroup_visit, &data, AVH_INFIX);
+    pthread_rwlock_unlock(&(tests->rwlock));
+    /* ---------------------------------------------------------------------*/
+
+    return ret == AVS_FINISHED ? 0 : -1;
+}
+
+/* ************************************************************************ */
+log_t *                 tests_getlog(
+                            testpool_t *        tests,
+                            const char *        testname) {
+    unsigned int    flags;
+    log_t *         log;
+
+    if (tests == NULL) {
+        return NULL;
+    }
+    if ((tests->flags & TPF_LOGTRUEPREFIX) != 0) {
+        flags = LPG_TRUEPREFIX;
+    } else {
+        flags = LPG_DEFAULT;
+        testname = TESTPOOL_LOG_PREFIX;
+    }
+    log = logpool_getlog(tests->logs, testname, flags);
+
+    return log;
+}
+
+/* ************************************************************************ */
+testgroup_t *           tests_start(
+                            testpool_t *        tests,
+                            const char *        testname) {
+
+    testgroup_t *   group;
+
+    if (tests == NULL) {
+        return NULL;
+    }
+    if ((group = calloc(1, sizeof(testgroup_t))) == NULL) {
+        return NULL;
+    }
+
+    group->testpool = tests;
+    group->flags = tests->flags;
+    group->n_tests = group->n_ok = group->n_errors = 0;
+    group->log = tests_getlog(tests, testname);
+    group->name = strdup(testname);
+
+    pthread_rwlock_wrlock(&(tests->rwlock));
+
+    if (avltree_insert(tests->tests, group) == NULL) {
+        tests_group_free(group);
+        group = NULL;
+    }
+
+    pthread_rwlock_unlock(&(tests->rwlock));
+
+    BENCH_TM_START(group->tm_bench);
+    BENCH_START(group->cpu_bench);
+
+    return group;
+}
+
+/* ************************************************************************ */
+unsigned long           tests_end(
+                            testgroup_t *       testgroup) {
+    if (testgroup == NULL) {
+        return 1;
+    }
+
+    BENCH_STOP(testgroup->cpu_bench);
+    BENCH_TM_STOP(testgroup->tm_bench);
+
+    return testgroup->n_errors;
+}
+
+/* ************************************************************************ */
+int                     tests_check(
+                            testresult_t *      result,
+                            const char *        func,
+                            const char *        file,
+                            int                 line) {
+    int store_result;
+
+    if (result == NULL || result->testgroup == NULL) {
+        return -1;
+    }
+    result->id = (result->testgroup->n_tests)++;
+    if (result->success) {
+        ++(result->testgroup->n_ok);
+        store_result = ((result->testgroup->flags & TPF_STORE_RESULTS) != 0);
+    } else {
+        ++(result->testgroup->n_errors);
+        store_result = ((result->testgroup->flags & (TPF_STORE_ERRORS | TPF_STORE_RESULTS)) != 0);
+    }
+    if (store_result) {
+        testresult_t * newresult = malloc(sizeof(*result));
+        if (newresult != NULL) {
+            memcpy(newresult, result, sizeof(*result));
+            newresult->func = strdup(func);
+            newresult->file = strdup(file);
+            newresult->line = line;
+            newresult->msg  = strdup(result->msg);
+            newresult->checkname = strdup(result->checkname);
+            result->testgroup->results = slist_prepend(result->testgroup->results, newresult);
+        }
+    }
+    return result->success;
+}
+
+/* ************************************************************************ */
+
